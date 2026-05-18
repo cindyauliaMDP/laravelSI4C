4e597c4f12e13188faaa942e396566906308ecf2
cindyauliaMDP <cindyaulia_2428240134@mhs.mdp.ac.id>
2026-05-18 13:10:58 +0700

    Senin18Mei

--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,27 +1,35 @@
 @extends('main')
 
-@section('title', 'Program Studi')
+@section('title')
+    <h1>Data Prodi</h1>
+@endsection
 
 @section('content')
+    <a href={{ route('prodi.create') }} class="btn btn-primary mb-3">Tambah Prodi</a>
+    <table class="table table-border table-hover" border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Nama Prodi</th>
+            <th>Singkatan</th>
+            <th>Kaprodi</th>
+            <th>Nama Fakultas</th>
+            <th>Kode Fakultas</th>
+            <th>Dekan Fakultas</th>
+        </tr>
+        @foreach ($prodis as $p)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $p->nama_prodi }}</td>
+                <td>{{ $p->singkatan }}</td>
+                <td>{{ $p->kaprodi }}</td>
+                <td>{{ $p->fakultas->nama_fakultas }}</td>
+                <td>{{ $p->fakultas->kode_fakultas }}</td>
+                <td>{{ $p->fakultas->dekan_fakultas }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
 
-<table class="table table-bordered table-hover">
-    <tr>
-        <th>No</th>
-        <th>Nama Prodi</th>
-        <th>Singkatan</th>
-        <th>Kaprodi</th>
-        <th>Fakultas</th>
-    </tr>
-
-    @foreach($prodis as $key => $prodi)
-    <tr>
-        <td>{{ $key + 1 }}</td>
-        <td>{{ $prodi->nama_prodi }}</td>
-        <td>{{ $prodi->singkatan }}</td>
-        <td>{{ $prodi->kaprodi }}</td>
-        <td>{{ $prodi->fakultas->nama ?? '-' }}</td>
-    </tr>
-    @endforeach
-
-</table>
+@section('footer')
+    <p>RyHar </p>
 @endsection
