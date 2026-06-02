273cbe69700d25b7a713ef68bc2cb2b57ec9315a
cindyauliaMDP <cindyaulia_2428240134@mhs.mdp.ac.id>
2026-06-02 13:10:56 +0700

    selasa,2 juni

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        // DB
-        $jumlahMahasiswa = DB::select('SELECT p.nama, count(*) as jumlah
-        from laravel.mahasiswas m
-        join laravel.prodis p
-        on m.prodi_id =p.id
-        group by p.nama_prodi');
-        return view('dashboard.index', compact('jumlahMahasiswa'));
+        $grafikmhs = DB::select("SELECT prodis.nama_prodi, 
+                                COUNT(*) as jumlah_mhs 
+                                FROM mahasiswas
+                                JOIN prodis 
+                                ON mahasiswas.prodi_id = prodis.id
+                                GROUP BY prodis.nama_prodi");
+
+        $angkatanData = DB::select("SELECT left(npm,2) as angkatan, COUNT(*) as total 
+                                FROM mahasiswas
+                                GROUP BY left(npm,2)");
+
+        return view('dashboard', compact('grafikmhs', 'angkatanData'));
     }
 }
