8b1fb4cd676de6ff591954097a1d96fc28e19924
cindyauliaMDP <cindyaulia_2428240134@mhs.mdp.ac.id>
2026-06-09 13:02:11 +0700

    cleverCloud
    9 juni

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/testview', function () {
-    return view('dashboard');
-});
-
 require __DIR__.'/auth.php';
